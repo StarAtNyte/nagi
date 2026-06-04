13aa4500a8fe629a21e9de7f3d1a179bd01df794
StarAtNyte <khanalnitij20@gmail.com>
2026-06-05 01:15:02 +0545

    fix: extract mpv.lib for MSVC linker, search wix/ on Windows

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
 fn main() {
-    // Project root contains libmpv.so symlink; also check system lib path
-    println!("cargo:rustc-link-search=native={}", env!("CARGO_MANIFEST_DIR"));
-    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search=native=/usr/lib");
+    let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    match target_os.as_str() {
+        "linux" => {
+            // libmpv.so symlink in project root; also search system paths
+            println!("cargo:rustc-link-search=native={}", manifest);
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+            println!("cargo:rustc-link-search=native=/usr/lib");
+        }
+        "windows" => {
+            // mpv.lib (MSVC import library) placed in wix/ by CI
+            println!("cargo:rustc-link-search=native={}/wix", manifest);
+        }
+        _ => {}
+    }
 }
